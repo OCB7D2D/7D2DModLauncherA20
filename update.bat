a3807c9f7e48167da1c473f711411c1d71de9c80
Marcel Greter <marcel.greter@ocbnet.ch>
2022-01-11 11:14:58 +0100

    Update ElectricityOverhaul to version 0.8.0

---
+++ update.bat
@@ -0,0 +1,56 @@
+cd ClaimAutoRepair
+git fetch
+git checkout tags/0.7.1
+cd ..
+
+cd ElectricityButtonsPush
+git fetch
+git checkout tags/0.5.0
+cd ..
+
+cd ElectricityLamps
+git fetch
+git checkout tags/0.7.0
+cd ..
+
+cd ElectricityLampsPOI
+git fetch
+git checkout tags/0.5.0
+cd ..
+
+cd ElectricityLightFixes
+git fetch
+git checkout tags/0.1.0
+cd ..
+
+cd ElectricityLightShadowSoft
+git fetch
+git checkout tags/0.3.0
+cd ..
+
+cd ElectricityNoWires
+git fetch
+git checkout tags/0.9.0
+cd ..
+
+cd ElectricityOverhaul
+git fetch
+git checkout tags/0.8.0
+cd ..
+
+cd ElectricitySolarRecipes
+git fetch
+git checkout tags/0.8.0
+cd ..
+
+cd ElectricityWireColors
+git fetch
+git checkout tags/0.9.0
+cd ..
+
+cd ElectricityWorkarounds
+git fetch
+git checkout tags/0.2.0
+cd ..
+
+pause
